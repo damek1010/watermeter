c5a1f17b08859218885dd361cfe5d7e0eedc0146
wiktor145 <viktor145@wp.pl>
2019-11-17 15:49:41 +0100

    how to get a result

--- QueryExecutor.cpp
+++ QueryExecutor.cpp
@@ -37,7 +37,35 @@ int QueryExecutor::createDatabase() {
 int QueryExecutor::createAndObtainId(char* sql) {
 
     if (create(sql) == SQLITE_OK){
-        //get id
+        /*
+        String sql = "Select year, state, name, total_babies, primary_sex, primary_sex_ratio, \
+                    per_100k_in_state from gendered_names \
+                    where name between 'Bob' and 'Bobby'";
+        rc = sqlite3_prepare_v2(db1, sql.c_str(), 1000, &res, &tail);
+        if (rc != SQLITE_OK) {
+            String resp = "Failed to fetch data: ";
+            resp += sqlite3_errmsg(db1);
+            resp += "<br><br><a href='/'>back</a>";
+            Serial.println(resp.c_str());
+            return;
+        }
+
+        while (sqlite3_step(res) == SQLITE_ROW) {
+            Serial.printf("Year: %d\n", sqlite3_column_int(res, 0));
+            Serial.printf("State: %s\n", (const char *) sqlite3_column_text(res, 1));
+            Serial.printf("Name: %s\n", (const char *) sqlite3_column_text(res, 2));
+            Serial.printf("Total babies: %d\n", sqlite3_column_int(res, 3));
+            Serial.printf("Primary sex: %s\n", (const char *) sqlite3_column_text(res, 4));
+            Serial.printf("Ratio: %lf\n", sqlite3_column_double(res, 5));
+            Serial.printf("Per 100k in state: %lf\n\n", sqlite3_column_double(res, 6));
+            rec_count++;
+        }
+        Serial.printf("No. of records: %d", rec_count);
+
+        sqlite3_finalize(res);
+
+        sqlite3_close(db1);
+        */
     }
     return -1;
 }
